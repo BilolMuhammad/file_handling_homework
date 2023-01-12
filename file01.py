7dc370604a675be2aae0bbbfb64f22ea148b8edc
PRESTIGE <mamatqulov@gmail.com>
2023-01-12 09:38:33 +0500

    o1

--- file01.py
+++ file01.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data: str):
     """
     The data is from the file. Return data as a list type.
     Args:
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    list = []
+    rd = data.read().split(',')
+    for i in rd:
+        list.append(int(i))
+    return list
+
 
 # Read data from file
+f = open('txt_file/data01.txt', encoding='utf-8')
+print(main(data=f))
