788abaea74044c4d1551a65454da2d77347498f2
PRESTIGE <mamatqulov@gmail.com>
2023-01-12 12:37:11 +0500

    o5

--- file05.py
+++ file05.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data: str):
     """
     The data is from the file. Find the number of digital and str(non-digital) data and return as list type.
     Args:
@@ -6,5 +6,18 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    file = open(data)
+    rd = file.read().strip()
+    num = 0
+    nonnum = 0
+    for r in rd:
+        if r.isdigit():
+            num += 1
+        else:
+            nonnum += 1
+    return [num, nonnum]
+
+
 # Read data from file
+file = 'txt_file/data05.txt'
+print(type(main(file)))
