4e7e73635a9d16f36a2fa52f71bde9580649372e
PRESTIGE <mamatqulov@gmail.com>
2023-01-12 09:52:40 +0500

    o2

--- file02.py
+++ file02.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data: str):
     """
     The data is from the file. Return number of characters in the file.
     Args:
@@ -6,5 +6,11 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    f = open(data)
+    dt = f.read()
+    return len(dt)
 
+
+link = 'txt_file/data02.txt'
+print(main(link))
 # Read data from file
