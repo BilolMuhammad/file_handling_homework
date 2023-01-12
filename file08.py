64412599891c16e0b7c22a4acb5428a1aacf6134
PRESTIGE <mamatqulov@gmail.com>
2023-01-12 12:46:55 +0500

    o8

--- file08.py
+++ file08.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data: str):
     """
     The data is from the file. Find the largest of the numeric characters.
     Args:
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    file = open(data)
+    rd = file.read()
+    max = 0
+    for r in rd:
+        if r.isdigit():
+            if int(r) > max:
+                max = int(r)
+    return max
+
 
 # Read data from file
+file = 'txt_file/data08.txt'
+print(main(file))
