a273b6efcd694057fecad8f1a90046177a1c4ab2
PRESTIGE <mamatqulov@gmail.com>
2023-01-12 13:35:36 +0500

    1o

--- file10.py
+++ file10.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data: str):
     """
     The data is from the file. Find the each row length and return the largest row.
     Args:
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    file = open(data, encoding='utf-8')
+    rd = file.read().split('\n')
+    leng = []
+    for r in rd:
+        leng.append(len(r))
+    return max(leng)
+
 
 # Read data from file
+link = 'txt_file/data10.txt'
+print(main(link))
