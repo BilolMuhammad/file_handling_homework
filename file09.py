f5611466d7411bca0fc327d1bbd9508ea3b09f08
PRESTIGE <mamatqulov@gmail.com>
2023-01-12 12:47:14 +0500

    o9

--- file09.py
+++ file09.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data: str):
     """
     The data is from the file. Find the smallest of the numeric characters.
     Args:
@@ -6,5 +6,19 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    file = open(data)
+    rd = file.read()
+    list = []
+    for r in rd:
+        if r.isnumeric():
+            list.append(int(r))
+    min = list[0]
+    for l in list:
+        if l < min:
+            min = l
+    return min
+
 
 # Read data from file
+link = 'txt_file/data09.txt'
+print(main(link))
