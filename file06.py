5c675ec429cdb0da31533c9de5523ffe7eb235e8
PRESTIGE <mamatqulov@gmail.com>
2023-01-12 12:40:59 +0500

    o6

--- file06.py
+++ file06.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data: str):
     """
     The data is from the file. Find the each row length and return as list type.
     Args:
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    file = open(data)
+    rd = file.read().split()
+    list = []
+    for r in rd:
+        list.append(len(r))
+
+    return list
+
+
 # Read data from file
+link = 'txt_file/data06.txt'
+print(main(link))
