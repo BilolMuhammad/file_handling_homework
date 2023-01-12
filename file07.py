ebb815c7a8fc05113ceee97429d274bfdefc5dd0
PRESTIGE <mamatqulov@gmail.com>
2023-01-12 12:44:28 +0500

    o7

--- file07.py
+++ file07.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data: str):
     """
     The data is from the file. Find a sum of numeric characters and return as list type.
     Args:
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    file = open(data)
+    rd = file.read()
+    n_sum = 0
+    for r in rd:
+        if r.isdigit():
+            n_sum += int(r)
+    return n_sum
+
+
 # Read data from file
+link = 'txt_file/data07.txt'
+print(main(link))
