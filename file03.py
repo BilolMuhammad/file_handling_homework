26f0eb1e25226e7b26d13665d6ae9a5897a55ab7
PRESTIGE <mamatqulov@gmail.com>
2023-01-12 10:01:01 +0500

    o3

--- file03.py
+++ file03.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data: str):
     """
     The data is from the file. Return the numbers as a list type.
     Args:
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    file = open(data)
+    rd = file.read()
+    list = []
+    for l in rd:
+        if l.isdigit():
+            list.append(int(l))
+
+    return list
+
+
 # Read data from file
+link = 'txt_file/data03.txt'
+print(main(link))
